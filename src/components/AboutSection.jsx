0c590491f3d7bf7aa3acdfc386daafb1e31e67d4
udara <chamiduudara321@gmail.com>
2025-08-07 16:30:13 +0530

    cv updated

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -33,7 +33,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/CV_Udara_Herath.pdf"
+                href="/cv_udara.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
